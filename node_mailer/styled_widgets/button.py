8e29b76b88f2bc53f6a8a3b35b2b38b117773329
Mervin van Brakel <63094424+mervinvb@users.noreply.github.com>
2024-11-13 15:41:38 +0100

    Updates button docstrings

--- node_mailer/styled_widgets/button.py
+++ node_mailer/styled_widgets/button.py
@@ -11,13 +11,21 @@ class NodeMailerButton(QtWidgets.QPushButton):
     """Window 95 styled button!"""
 
     def __init__(self, text: str) -> None:
-        """Initializes the button."""
+        """Initializes the button.
+
+        Args:
+            text: The text to display on the button.
+        """
         super().__init__()
         self.configure_styling(text)
         self.connect_icon_changing_signals()
 
-    def configure_styling(self, text: str):
-        """Loads the icons and sets the button styling."""
+    def configure_styling(self, text: str) -> None:
+        """Loads the icons and sets the button styling.
+
+        Args:
+            text: The text to display on the button.
+        """
         self.button_icon = QtGui.QIcon(
             str(Path(__file__).parent.parent / "resources" / "button.png")
         )
@@ -32,20 +40,24 @@ class NodeMailerButton(QtWidgets.QPushButton):
         self.setStyleSheet("background: transparent; border: none; font: 11pt 'W95FA';")
         self.setFixedSize(80, 24)
 
-    def connect_icon_changing_signals(self):
+    def connect_icon_changing_signals(self) -> None:
         """Connects the signals so the icon changes when needed."""
         self.pressed.connect(self.change_icon_to_pressed)
         self.released.connect(self.change_icon_to_normal)
 
-    def change_icon_to_pressed(self):
+    def change_icon_to_pressed(self) -> None:
+        """Changes the icon to make the button look pressed."""
         self.active_icon = self.pressed_button_icon
         self.repaint()
 
-    def change_icon_to_normal(self):
+    def change_icon_to_normal(self) -> None:
+        """Changes the icon to make the button look normal."""
         self.active_icon = self.button_icon
         self.repaint()
 
-    def paintEvent(self, event):  # noqa: N802
+    def paintEvent(self, _) -> None:  # noqa: N802
+        """Overwrites the default paint event to draw our button text
+        on top of our custom button icons."""
         painter = QtGui.QPainter(self)
         icon_rect = self.rect()
         icon = self.active_icon.pixmap(self.iconSize())
